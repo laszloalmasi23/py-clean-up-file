ffb2be7adacb5e92aeab06462c0cbd28d52a6bd5
laszloalmasi23 <laszlo.almasi7@gmail.com>
2026-02-20 13:56:08 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> "CleanUpFile":
+    def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
